82329a65781328b0ed4e650c53badd20653cac3c
Illya G <igordus@jaumebalmes.net>
2022-11-03 13:43:07 +0100

    B-2 - Header redesign

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,57 @@
-import styles from '../styles/Header.module.scss'
 import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const header = () => {
     return (
-        <div className={styles.header}>
-            <div>Crypto X Change</div>
-            <div className={styles.navigation}>
-                <Link href={'/'}><div>Inicio</div></Link>
-                <Link href={'/exchanges'}><div>Compra Criptomonedas</div></Link>
-            </div>
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#">CryptoXChange</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/exchanges">Comprar Criptomonedas</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
+
+/* 
+
+DROPDOWNS
+
+<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+    <NavDropdown.Item href="#action/3.2">
+        Another action
+    </NavDropdown.Item>
+    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+    <NavDropdown.Divider />
+    <NavDropdown.Item href="#action/3.4">
+        Separated link
+    </NavDropdown.Item>
+</NavDropdown>
+
+*/
 export default header;
