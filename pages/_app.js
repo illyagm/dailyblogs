254398f390b96d1560a8f05610318014400a087d
illyagm <igordus@jaumebalmes.net>
2022-11-07 18:56:45 +0100

    B-19 - Vercel analytics implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.scss'
 import 'bootstrap/dist/css/bootstrap.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
