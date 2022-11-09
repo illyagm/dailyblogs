1de11925d305ad5c1fdb841cd8404492962fa666
illyagm <igordus@jaumebalmes.net>
2022-11-09 21:28:08 +0100

    B-2 - Header redesign

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styles from '../styles/header/Header.module.scss';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
 // Component import
 import CryptoPrices from './CryptoPrices';
 import WelcomeComponent from '../components/HomeWelcome';
@@ -12,9 +13,21 @@ const Header = () => {
     const router = useRouter()
     return (
         <Row className={router.pathname == '/' ? '' : 'g-0'}>
-            <Navbar bg="light" expand="lg" className={styles.header}>
-                <Container fluid>
-                    <Navbar.Brand href="/" className={styles.title}>CryptoXChange</Navbar.Brand>
+            <Navbar bg="dark" variant="dark" expand="lg" className={styles.header}>
+                <Container>
+                    <Navbar.Brand href="/" className={styles.title}>
+                        Crypt
+                        <CurrencyBitcoinIcon 
+                        sx={{ 
+                            backgroundColor: '#E2B658',
+                            fill: '#202528',
+                            borderRadius: '50%',
+                            fontSize: '15px !important',
+                            margin: '0.1rem 0.1rem'
+
+                        }}/>
+                        XChange
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
@@ -22,8 +35,8 @@ const Header = () => {
                             style={{ maxHeight: '100px' }}
                             navbarScroll
                         >
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/exchanges">Exchanges</Nav.Link>
+                            <Nav.Link className={ router.pathname == '/' ? 'active' : ''} href="/">Home</Nav.Link>
+                            <Nav.Link className={ router.pathname == '/exchanges' ? 'active' : ''} href="/exchanges">Exchanges</Nav.Link>
                         </Nav>
                         <Nav>
                             <Nav.Link className='justify-content-end'><CryptoPrices /></Nav.Link>
