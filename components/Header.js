1f441a5b323e63fe353d942b239c96f769800b4f
illyagm <igordus@jaumebalmes.net>
2023-01-22 12:28:30 +0100

    Hotfix - Small static content changes

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styles from '../styles/header/Header.module.scss';
 import { useRouter } from 'next/router';
-import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
+//import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
 // Component import
 import CryptoPrices from './CryptoPrices';
 import WelcomeComponent from '../components/HomeWelcome';
@@ -16,8 +16,8 @@ const Header = () => {
             <Navbar bg="dark" variant="dark" expand="lg" className={styles.header}>
                 <Container>
                     <Navbar.Brand href="/" className={styles.title}>
-                        Crypt
-                        <CurrencyBitcoinIcon
+                        Crypto
+                        {/* <CurrencyBitcoinIcon
                             sx={{
                                 backgroundColor: '#E2B658',
                                 fill: '#202528',
@@ -25,7 +25,7 @@ const Header = () => {
                                 fontSize: '15px !important',
                                 margin: '0.1rem 0.1rem'
 
-                            }} />
+                            }} /> */}
                         XChange
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
@@ -35,14 +35,14 @@ const Header = () => {
                             style={{ maxHeight: '100px' }}
                             navbarScroll
                         >
-                            <Nav.Link className={router.pathname == '/' ? 'active' : ''} href="/">Home</Nav.Link>
-                            <Nav.Link className={router.pathname == '/exchanges' ? 'active' : ''} href="/exchanges">Exchanges</Nav.Link>
+                            <Nav.Link className={router.pathname == '/' ? 'active' : ''} href="/">Inicio</Nav.Link>
+                            <Nav.Link className={router.pathname == '/exchanges' ? 'active' : ''} href="/exchanges">Plataformas</Nav.Link>
                         </Nav>
                         <Nav>
                             <Nav.Link className='justify-content-end'><CryptoPrices /></Nav.Link>
                         </Nav>
                         <Nav className={styles.buttonExchange}><Button href="https://www.binance.com/en/activity/referral-entry/CPA?fromActivityPage=true&ref=CPA_00ELJGV9UV" target="_blank" variant='secondary'>
-                            ¡100$ de bono en 1era inversión!
+                            ¡Invierte y llévate <b>100$</b> de bono!
                         </Button></Nav>
                     </Navbar.Collapse>
                 </Container>
