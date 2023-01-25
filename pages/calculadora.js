2cc3033b1e4380b99f45d0c5f555654b44daaa28
illyagm <igordus@jaumebalmes.net>
2023-01-25 23:32:27 +0100

    B-13 - SEO fixes

--- pages/calculadora.js
+++ pages/calculadora.js
@@ -1,8 +1,15 @@
-import { Container } from "react-bootstrap"
 import CalculatorComponent from "../components/IncomeCalculator";
+import Head from 'next/head'
+
 const StakingCalculator = () => {
     return (
         <>
+            <Head>
+                <title>Calculadora Staking</title>
+                <meta name="description"
+                    content="¡Calculadora para calcular tus beneficios de staking invirtiendo en Bitcoin y Ethereum!" />
+                <link rel="icon" href="/logoCXC.png" />
+            </Head>
             <CalculatorComponent />
         </>
     )
