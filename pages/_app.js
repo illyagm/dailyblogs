dac71c07e19b15627ae1e1bf93a153d8b3afd1ba
Illya G. <igordus@jaumebalmes.net>
2024-05-12 14:39:35 +0200

    [X-2] - Cookie banner implemented

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { Analytics } from '@vercel/analytics/react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import CookieConsent from "react-cookie-consent";
+import styles from "../styles/base/base.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
-      <Analytics />
-      <Footer />
+        <Header />
+        <Component {...pageProps} />
+        <CookieConsent
+          buttonText="Entendido"
+          buttonStyle={{fontWeight: "normal", backgroundColor: "#E2B658"}}
+          style={{textAlign: "center"}}
+          className={styles.cookieConsent}
+        >Este sitio web utiliza cookies para mejorar la experiencia de usuario. Continuando aquí aceptas las condiciones.</CookieConsent>
+        <Analytics />
+        <Footer />
     </>
   )
 }
